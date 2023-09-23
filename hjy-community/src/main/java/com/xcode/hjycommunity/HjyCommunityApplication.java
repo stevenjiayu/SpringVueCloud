106e92da12f8d2be671993c78aa99eca7f477cf7
stevenwu <stevenjiayu1229@gmail.ocm>
2023-09-23 14:35:28 +0800

    Project Test2

---
+++ hjy-community/src/main/java/com/xcode/hjycommunity/HjyCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.xcode.hjycommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjyCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HjyCommunityApplication.class, args);
+    }
+
+}
---
+++ hjy-community/src/test/java/com/xcode/hjycommunity/HjyCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xcode.hjycommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HjyCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
